b175494804290e7458ea707e2200023ee0b4d9d7
IshaSharma <isha291001@gmail.com>
2024-05-20 23:14:23 +0530

    Task Completed

---
+++ RailWorld Training/src/DAY14_20MAY/LambdaExpressionEx5.java
@@ -0,0 +1,29 @@
+package DAY14_20MAY;
+
+public class LambdaExpressionEx5 {
+    public static void main(String[] args) {
+
+        //Thread Example without lambda
+        // creating runnable interface object
+        // and overriding run function using
+        // anonymous class.
+
+        Runnable t1 = new Runnable(){
+            public void run(){
+                System.out.println("This is thread1...");
+            }
+        };
+
+        Thread thread1=new Thread(t1);
+        thread1.start();
+        //Thread Example with lambda
+        // using Lambda to override the function.
+
+        Runnable t2 = () -> {
+            System.out.println("This is thread2...");
+        };
+
+        Thread thread2 = new Thread(t2);
+        thread2.start();
+    }
+}
---
+++ RailWorld Training/src/DAY14_20MAY/Lambdaa.java
@@ -0,0 +1,18 @@
+package DAY14_20MAY;
+//Without Lambda Expression
+interface Drawable{
+    public void draw();
+}
+class Lambdaa {
+    public static void main(String[] args) {
+        int width = 10;
+
+        //without lambda, Drawable implementation using anonymous class
+        Drawable d = new Drawable() {
+            public void draw() {
+                System.out.println("Drawing " + width);
+            }
+        };
+        d.draw();
+    }
+}
