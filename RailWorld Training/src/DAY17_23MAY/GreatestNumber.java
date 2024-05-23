26fd1839d2a9dd023edabec0531d3268f80fd740
IshaSharma <isha291001@gmail.com>
2024-05-23 22:28:48 +0530

    Task Completed

---
+++ RailWorld Training/src/DAY17_23MAY/GreatestNumber.java
@@ -0,0 +1,12 @@
+package DAY17_23MAY;
+public class GreatestNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        int greatest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+        System.out.println("The greatest number among " + a + ", " + b + ", and " + c + " is: " + greatest);
+    }
+}
---
+++ RailWorld Training/src/DAY17_23MAY/IntegerToBinary.java
@@ -0,0 +1,13 @@
+package DAY17_23MAY;
+public class IntegerToBinary {
+    public static void main(String[] args) {
+        int number = 67;
+
+        // Convert integer to binary string
+        String binaryString = Integer.toBinaryString(number);
+
+        // Print the original integer and its binary representation
+        System.out.println("Original value: " + number);
+        System.out.println("Binary representation: " + binaryString);
+    }
+}
